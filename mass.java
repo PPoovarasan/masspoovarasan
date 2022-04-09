ce480cf76f8ebc28fd9ff73f54e869311878c1ee
POOVARASAN P <102451428+PPoovarasan@users.noreply.github.com>
2022-04-09 06:18:45 -0700

    Add files via upload

---
+++ mass.java
@@ -0,0 +1,26 @@
+package poovarasan;
+import java.util.*;
+public class mass
+{
+ 
+	public static void main(String[] args) 
+{
+		// TODO Auto-generated method stub
+		
+		    
+		        Scanner sc=new Scanner(System.in);
+		        int n=sc.nextInt();
+		        for(int i=n;i>=1;i--)
+		        {
+		            for(int j=1;j<2*i;j++)
+		            {
+		                System.out.print("*");
+		            }
+		            System.out.println();
+		        }
+		        {
+		        	System.out.println("POOVARASAN P");
+		        }
+}
+}
+	
